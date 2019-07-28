32a827b075c932ddbf72116ad496735b1123cf0f
lurenjia528 <816837404@qq.com>
2019-07-28 08:23:55 +0800

    修复docker logs websocket 前端乱码问题,引用xterm

--- src/main/java/websocket/DockerLogsWebSocketServer.java
+++ src/main/java/websocket/DockerLogsWebSocketServer.java
@@ -24,31 +24,33 @@ import org.springframework.stereotype.Component;
 /**
  * docker logs -f
  */
-@ServerEndpoint("/websocket/{sid}")
+@ServerEndpoint("/log/{sid}/{containerId}")
 @Component
 @Slf4j
-public class WebSocketServer {
+public class DockerLogsWebSocketServer {
     //静态变量，用来记录当前在线连接数。应该把它设计成线程安全的。
     private static int onlineCount = 0;
     //concurrent包的线程安全Set，用来存放每个客户端对应的MyWebSocket对象。
-    private static CopyOnWriteArraySet<WebSocketServer> webSocketSet = new CopyOnWriteArraySet<WebSocketServer>();
+    private static CopyOnWriteArraySet<DockerLogsWebSocketServer> webSocketSet = new CopyOnWriteArraySet<DockerLogsWebSocketServer>();
 
     //与某个客户端的连接会话，需要通过它来给客户端发送数据
     private Session session;
 
     //接收sid
     private String sid = "";
+    private String cid = "";
 
     /**
      * 连接建立成功调用的方法
      */
     @OnOpen
-    public void onOpen(Session session, @PathParam("sid") String sid) {
+    public void onOpen(Session session, @PathParam("sid") String sid,@PathParam("containerId") String cid) {
         this.session = session;
         webSocketSet.add(this);     //加入set中
         addOnlineCount();           //在线数加1
         log.info("有新窗口开始监听:" + sid + ",当前在线人数为" + getOnlineCount());
         this.sid = sid;
+        this.cid = cid;
         try {
             sendMessage("连接成功");
         } catch (IOException e) {
@@ -75,7 +77,7 @@ public class WebSocketServer {
     public void onMessage(String message, Session session) {
         log.info("收到来自窗口" + sid + "的信息:" + message);
         //群发消息
-        for (WebSocketServer item : webSocketSet) {
+        for (DockerLogsWebSocketServer item : webSocketSet) {
             try {
                 item.sendMessage(message);
             } catch (IOException e) {
@@ -100,7 +102,7 @@ public class WebSocketServer {
      */
     public void sendMessage(String message) throws IOException {
 //        for(int i=0;i<100;i++){
-        String url = "http://harbor.ygt.cn:4243/containers/8e704c37956a/logs?stdout=true&follow=true";
+        String url = "http://harbor.ygt.cn:4243/containers/" + cid + "/logs?stdout=true&follow=true&tail=50";
         OkHttpClient client = new OkHttpClient.Builder().connectTimeout(10, TimeUnit.SECONDS).readTimeout(30, TimeUnit.SECONDS).build();
         RequestBody body = null;
         Request request = new Request.Builder()
@@ -128,7 +130,7 @@ public class WebSocketServer {
      */
     public static void sendInfo(String message, @PathParam("sid") String sid) throws IOException {
         log.info("推送消息到窗口" + sid + "，推送内容:" + message);
-        for (WebSocketServer item : webSocketSet) {
+        for (DockerLogsWebSocketServer item : webSocketSet) {
             try {
                 //这里可以设定只推送给这个sid的，为null则全部推送
                 if (sid == null) {
@@ -147,10 +149,10 @@ public class WebSocketServer {
     }
 
     public static synchronized void addOnlineCount() {
-        WebSocketServer.onlineCount++;
+        DockerLogsWebSocketServer.onlineCount++;
     }
 
     public static synchronized void subOnlineCount() {
-        WebSocketServer.onlineCount--;
+        DockerLogsWebSocketServer.onlineCount--;
     }
 }
